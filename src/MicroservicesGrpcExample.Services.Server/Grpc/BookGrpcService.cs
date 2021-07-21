66688d587f079f2367dfbaba172add355e6268c9
Nikolai Guscharin <nagushcharin@gmail.com>
2021-07-21 13:37:47 +0300

    Protos

--- src/MicroservicesGrpcExample.Services.Server/Grpc/BookGrpcService.cs
+++ src/MicroservicesGrpcExample.Services.Server/Grpc/BookGrpcService.cs
@@ -1,8 +1,14 @@
-﻿using MicroservicesGrpcExample.Services.Server.Protos;
+﻿using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using MicroservicesGrpcExample.Services.Server.Protos;
 
 namespace MicroservicesGrpcExample.Server.Grpc
 {
     public class BookGrpcService : BookStore.BookStoreBase
     {
+        public override Task<Empty> GetAll(Empty request, ServerCallContext context)
+        {
+        }
     }
 }
